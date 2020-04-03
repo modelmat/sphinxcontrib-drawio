008828bc04d160518b88ab86c4563b33b9b2a26e
Modelmat <modelmat@outlook.com.au>
2020-04-04 10:56:17 +1100

    Add test for :page-index:

---
+++ tests/roots/test-page-index/index.rst
@@ -0,0 +1,14 @@
+.. drawio:: pages.drawio
+    :alt: Page #1
+    :page-index: 0
+
+.. drawio:: pages.drawio
+    :alt: Page #2
+    :page-index: 1
+
+.. drawio:: pages.drawio
+    :alt: Page #3
+    :page-index: 2
+
+.. drawio:: pages.drawio
+    :alt: no page number
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,8 +2,9 @@ import pytest
 from bs4 import BeautifulSoup, Tag
 
 
+@pytest.mark.sphinx("html", testroot="simple")
 @pytest.mark.parametrize('page', ['index.html', ], indirect=True)
-def test_index(page: BeautifulSoup):
+def test_simple(page: BeautifulSoup):
     # noinspection PyTypeChecker
     directive: Tag = page.find("img", {"class": "drawio"})
     assert directive["alt"] == "An Example"
