f64867039ca1f6dd75132e77600ec7b4b3581700
Brecht Machiels <brecht@mos6581.org>
2020-09-01 11:41:06 +0200

    Fix failing test_pdf tests
    Test using the same 'testroot' share the same directory. This can
    cause one test to be affected by another. Solution: set a unique
    'srcdir' for each test so that they run in their own directory. Also,
    make test names unique to avoid confusion.

--- tests/test_update.py
+++ tests/test_update.py
@@ -11,7 +11,7 @@ SIMPLE_EXPORTED_FNAME = "drawio-bf0f85b68784bab0e62bf5902f5a46b65d71ee70.png"
 
 
 # deprecated drawio directive test
-@pytest.mark.sphinx("html", testroot="simple")
+@pytest.mark.sphinx("html", testroot="simple", srcdir="notchanged")
 def test_notchanged(content: Sphinx, make_app_with_local_user_config):
     exported = content.outdir / "_images" / SIMPLE_EXPORTED_FNAME
     exported_timestamp = exported.stat().st_mtime
@@ -21,7 +21,7 @@ def test_notchanged(content: Sphinx, make_app_with_local_user_config):
 
 
 # deprecated drawio directive test
-@pytest.mark.sphinx("html", testroot="simple")
+@pytest.mark.sphinx("html", testroot="simple", srcdir="changed")
 def test_changed(content: Sphinx, make_app_with_local_user_config):
     source = Path(content.srcdir / "box.drawio")
     exported = content.outdir / "_images" / SIMPLE_EXPORTED_FNAME
@@ -32,7 +32,7 @@ def test_changed(content: Sphinx, make_app_with_local_user_config):
     assert exported.stat().st_mtime > exported_timestamp
 
 
-@pytest.mark.sphinx("html", testroot="image")
+@pytest.mark.sphinx("html", testroot="image", srcdir="image_notchanged")
 def test_image_notchanged(content: Sphinx, make_app_with_local_user_config):
     exported = content.outdir / "_images" / "box.svg"
     exported_timestamp = exported.stat().st_mtime
@@ -41,7 +41,7 @@ def test_image_notchanged(content: Sphinx, make_app_with_local_user_config):
     assert exported.stat().st_mtime == exported_timestamp
 
 
-@pytest.mark.sphinx("html", testroot="image")
+@pytest.mark.sphinx("html", testroot="image", srcdir="image_changed")
 def test_image_changed(content: Sphinx, make_app_with_local_user_config):
     box = Path(content.srcdir / "box.drawio")
     exported = content.outdir / "_images" / "box.svg"
