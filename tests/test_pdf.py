e0dccb515a2eecb13bbb000dad6da43235838b26
Brecht Machiels <brecht@mos6581.org>
2020-09-01 11:41:06 +0200

    Fix failing test_pdf tests
    Test using the same 'testroot' share the same directory. This can
    cause one test to be affected by another. Solution: set a unique
    'srcdir' for each test so that they run in their own directory. Also,
    make test names unique to avoid confusion.

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -6,27 +6,27 @@ import pytest
 
 
 # deprecated drawio directive test
-@pytest.mark.sphinx("latex", testroot="simple")
-def test_simple(legacy_tex_images: List[Path]):
+@pytest.mark.sphinx("latex", testroot="simple", srcdir="pdf_simple")
+def test_pdf_simple(legacy_tex_images: List[Path]):
     image, = legacy_tex_images
     assert image.basename() == "drawio-6cf867c6e94665d8489581a35c2d215220d6c152.pdf"
 
 
-@pytest.mark.sphinx("latex", testroot="image")
-def test_image(tex_images: List[Path]):
+@pytest.mark.sphinx("latex", testroot="image", srcdir="pdf_image")
+def test_pdf_image(tex_images: List[Path]):
     image, = tex_images
     assert image.basename() == "box.pdf"
 
 
 # deprecated drawio directive test
-@pytest.mark.sphinx("latex", testroot="simple")
-def test_crop(legacy_tex_images: List[Path]):
+@pytest.mark.sphinx("latex", testroot="simple", srcdir="pdf_crop")
+def test_pdf_crop(legacy_tex_images: List[Path]):
     image, = legacy_tex_images
     assert get_mediabox(image) == ("88.080002", "46.079998")
 
 
-@pytest.mark.sphinx("latex", testroot="image")
-def test_image_crop(tex_images: List[Path]):
+@pytest.mark.sphinx("latex", testroot="image", srcdir="pdf_image_crop")
+def test_pdf_image_crop(tex_images: List[Path]):
     image, = tex_images
     assert get_mediabox(image) == ("88.080002", "46.079998")
 
