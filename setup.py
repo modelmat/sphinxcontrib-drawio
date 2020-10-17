a9f9ac8c471678eca7d81de9eb04354a2921866e
Modelmat <modelmat@outlook.com.au>
2020-10-17 12:27:29 +1100

    Switch over to setup.cfg from setup.py and static versioning
    This removes the need to manually fetch file contents and is
    instead handled by setuptools natively.
    
    This also serves as the base for a change to static versioning,
    which allows us to :
    * build without a git repository
    * allows sphinx extension setup() to return a version
    * have a commit specifically for upping the package version
      and for tagging

--- setup.py
+++ setup.py
@@ -1,31 +1,5 @@
-import subprocess
-
 import setuptools
 
-# This will fail if something happens or if not in a git repository.
-# This is intentional.
-ret = subprocess.run("git describe --tags --abbrev=0", stdout=subprocess.PIPE,
-                     stderr=subprocess.PIPE, check=True, shell=True)
-version = ret.stdout.decode("utf-8").strip()
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
-    name="sphinxcontrib-drawio",
-    version=version,
-    author="Modelmat",
-    author_email="modelmat@outlook.com.au",
-    description="Sphinx Extension to include draw.io files",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Modelmat/sphinxcontrib-drawio",
-    packages=['sphinxcontrib'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Framework :: Sphinx :: Extension",
-    ],
-    python_requires='>=3.6',
+    # see setup.cfg
 )
---
+++ sphinxcontrib/__init__.py
@@ -0,0 +1,2 @@
+name="sphinxcontrib-drawio"
+__version__="0.10.0"
