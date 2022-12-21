67f13b2f72794ec7de4de9840c1b0db09105fdd7
Modelmat <modelmat@outlook.com.au>
2022-12-21 23:04:51 +1100

    Rework conversion handling to prevent subtle errors
    Error on invalid formats for both global & local config,
    for the drawio valid formats and valid builder formats.
    
    Changes imgconverter tests to verify no unnecessary conversions
    and no erroring when using that extension.

---
+++ tests/roots/test-bad-config2/conf.py
@@ -0,0 +1,9 @@
+extensions = ["sphinxcontrib.drawio"]
+
+master_doc = "index"
+exclude_patterns = ["_build"]
+
+# removes most of the HTML
+html_theme = "basic"
+
+drawio_builder_export_format = {"html": "pdf"}
