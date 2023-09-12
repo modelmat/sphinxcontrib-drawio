373b54f179b19a0a6dee2493e9333811869210cb
Jared Dillard <jared.dillard@joby.aero>
2023-09-12 09:44:53 -0700

    Bump version to 0.0.17

--- sphinxcontrib/drawio/__init__.py
+++ sphinxcontrib/drawio/__init__.py
@@ -25,7 +25,7 @@ from sphinx.util import logging
 from sphinx.util.docutils import SphinxDirective
 from sphinx.util.fileutil import copy_asset
 
-__version__ = "0.0.16"
+__version__ = "0.0.17"
 
 logger = logging.getLogger(__name__)
 
